e8e0ca371dbdca7f42a78628a99f7ce9379f5b82
emdadul38 <emdadul225@gmail.com>
2015-10-31 09:42:23 +0600

    Check value add with append and remove that checked value with specific class using jquery remove method

---
+++ checkAppend.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>
+    $(document).ready(function(){
+        $(".test").bind('click', function() {
+            var data = $(this).attr("data-type");
+            var dataId = $(this).attr("data-id");
+            var $t = $(this);
+            if ($t.is(':checked')) {
+            // append item
+            $('#items').append('<span class=test'+dataId+'>'+data+'</span>');
+            }else{
+                // empty div and remove it from DOM (empty helps with memory leak issues with $.fn.remove() )
+                $('#items').find('.test'+dataId).remove();
+            }
+        });
+    });
+</script>
+</head>
+<body>
+
+<p>This is a paragraph.</p>
+<p>This is another paragraph.</p>
+
+<ol>
+  <li>List item 1</li>
+  <li>List item 2</li>
+  <li>List item 3</li>
+</ol>
+<?php for ($i=0; $i <4 ; $i++) :?>
+    <input type="checkbox" class="test" data-id ="<?php echo $i+1; ?>"  data-type="NO <?php echo $i+1; ?>"/> Check me </br>
+<?php endfor; ?>
+
+<div id="items">
+
+</div>
+
+
+</body>
+</html>
